73e6c458c20823bbad8344f87a285e31e7ca9b36
xlqtest <2324656373@qq.com>
2020-06-01 11:01:58 +0800

    测试

---
+++ demo.py
@@ -0,0 +1,17 @@
+print("hello world")
+print（2654）
+print(2.679)
+print(None)
+print(True,False)
+print(())
+print([])
+print({})
+
+
+a="haha"
+print（a）
+
+
+
+
+print("\n".join([''.join([('LangJins'[(x-y)%8]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
